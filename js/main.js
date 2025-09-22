951115eb56e6d7a3e0758e53e32a02c728f51740
dgoldberg <dgoldberg@pandora.com>
2025-09-21 22:03:56 -0700

    text tweak

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
-const aboutText1 = `Feral Ecology is a project exploring foraging | fermentation | food as portals for human + ecological expression, healing, and connectivity. 
-It is an unfolding story of curious and whimsical humans with a twinkle in their eye 
-for finding joy and meaning where the dandelions break through the concrete.`;
+const aboutText1 = `Feral Ecology is where humans, nature, and food meet.  Through foraging, fermentation, and community building, we aim to build a direct relationship with the land, celebrate diverse traditions, and cultivate wholeness for a more resilient future.`;
+const aboutText2 = `We offer natural wines/ciders/meads, as well as revolving seasonal events -- community urban fruit harvests, workshops, guided forays, guerilla fruit tree planting, retreats, and more.`;
 
-const aboutText2 = `The gift: feral fruit! Wild mushrooms! Invasive wild boar! Acorn! Seaweeds, clams and mussels!`;
-const aboutText3 = `The salvation: your smile and the unexplainable glee in the mystery of creation 
-that brings people to discover fermentation, fire, song, togetherness and a reverence for the earth which we are made of and belong to.`;
+const aboutText3 = `The gift: feral fruit! Wild mushrooms! Acorns! Seaweed, clams and mussels! Invasive wild boar!`;
+const aboutText4 = `The salvation: your smile and the unfolding mystery
+that brings people to discover fermentation, fire, song, and reverence for the earth which we are made of and belong to.`;
 
-const aboutText4 = `Our offerings include wine, as well as a mix of events that revolve around the seasons. Community urban fruit harvests, educational hands on workshops, foraging outings, guerilla fruit tree planting, and more.`;
-const aboutText5 = `Daniel Goldberg has been finding expression through cooking and foraging since he was a wee lad. 
-He grew up in NYC, in a family of Soviet jewish immigrants, 
-hearing stories of his parents picking mushrooms in the woods. 
-
-He has been exploring the rich bay area landscape for 12 years: 
-foraging, fermenting, surfing, rock climbing, 
-and playing saxophone in various bands`;
+const aboutText5 = `Daniel Goldberg has been exploring cooking and foraging since he was a wee lad. 
+He grew up in NYC, in a family of Soviet Jewish immigrants, 
+hearing stories of his parents picking mushrooms in the woods, but has called the Bay Area home for over 12 years. 
+`;
 
 const events = [
   {
